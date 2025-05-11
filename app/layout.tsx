88a61223972230bd9174c112a697382281543dbf
Arisuuuuu <jamesxcasipong@gmail.com>
2025-05-11 23:42:49 +0800

    Add @next/third-parties and integrate Google Analytics
    Added the @next/third-parties package to package.json and package-lock.json. Refactored layout.tsx to replace the manual Google Analytics script with the GoogleAnalytics component from @next/third-parties for cleaner and more maintainable integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import {Toaster} from "sonner";
-import {Head} from "next/document";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: 'Pilipinas 2025',
@@ -20,24 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-        {/* Google Analytics script */}
-        <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-D7565NTJF8`}
-        ></script>
-        <script
-            dangerouslySetInnerHTML={{
-                __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-D7565NTJF8');
-              `,
-            }}
-        />
-    </head>
+
       <body>{children}
+      <GoogleAnalytics gaId="G-D7565NTJF8" />
       <Toaster />
       </body>
     </html>
